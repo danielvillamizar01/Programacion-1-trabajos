7f7d72f86203cbc4ed73a3c296ad87f43793cb71
Daniel <danielito20505@gmail.com>
2025-08-30 21:12:26 -0500

    cambios

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola amiguitos");
+        System.out.println("Daniel Felipe Villamizar Barcenas");
     }
 }
 
